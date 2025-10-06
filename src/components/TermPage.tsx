8d1ab4b495636dee0c3efe219e8b8d82da484f73
Kaival Shah <kaival.s.shah@gmail.com>
2025-10-06 18:39:46 -0500

    add modal for course selections

--- src/components/TermPage.tsx
+++ src/components/TermPage.tsx
@@ -1,21 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import TermSelector from "./TermSelector"
 import CourseList from "./CourseList";
-import type {Course} from "../types";
+import type { Course } from "../types";
+import Modal from "./Modal";
 
 interface TermPageProps {
     courses: Course[]
 }
 
-const TermPage = ({courses}: TermPageProps) => {
+const TermPage = ({ courses }: TermPageProps) => {
     const [term, setTerm] = useState("Fall")
     const [selectedCourses, setSelectedCourses] = useState<Course[]>([])
+    const [isModalSelected, setIsModalSelected] = useState(false);
 
     return (
-        <div>
-            {selectedCourses.map(course => course.title).join(", ")}
-            <TermSelector onChange={setTerm}/>
-            <CourseList courses={courses} term={term} selectedCourses={selectedCourses} onChange={setSelectedCourses}/>
+        <div className="p-3">
+            <div className="flex justify-between items-start px-4">
+                <TermSelector onChange={setTerm} />
+                <div className="flex justify-end">
+                    <button onClick={() => setIsModalSelected(!isModalSelected)} className="border-2 border-gray-400 border-solid rounded-[1vw] p-3 mb-3 w-32">
+                        Course Plan
+                    </button>
+                    <Modal isOpen={isModalSelected} onClose={() => setIsModalSelected(false)} children={selectedCourses.length !== 0 ?
+                        selectedCourses.map(course => 
+                            <div className="border border-3 p-3 mb-3 rounded-[1vw] align-center">
+                                {course.term} CS {course.number} | {course.title} | {course.meets}
+                            </div>
+                        ):
+                        "No courses have been selected yet. Click a course to select."} />
+                </div>
+            </div>
+            <CourseList courses={courses} term={term} selectedCourses={selectedCourses} onChange={setSelectedCourses} />
         </div>
     );
 }
--- src/components/TermSelector.tsx
+++ src/components/TermSelector.tsx
@@ -4,10 +4,10 @@ interface TermProps {
 
 const TermSelector = ({onChange}: TermProps) => {
     return (
-        <div className="grid grid-cols-3 pb-3">
-            <button className="border-2 border-gray-400 border-solid rounded-[1vw]" type="button" onClick={() => onChange("Fall")}>Fall</button>
-            <button className="border-2 border-gray-400 border-solid rounded-[1vw]" type="button" onClick={() => onChange("Winter")}>Winter</button>
-            <button className="border-2 border-gray-400 border-solid rounded-[1vw]" type="button" onClick={() => onChange("Spring")}>Spring</button>
+        <div className="grid grid-cols-3 gap-4">
+            <button className="border-2 border-gray-400 border-solid rounded-[1vw] p-3 mb-3 w-32" type="button" onClick={() => onChange("Fall")}>Fall</button>
+            <button className="border-2 border-gray-400 border-solid rounded-[1vw] p-3 mb-3 w-32" type="button" onClick={() => onChange("Winter")}>Winter</button>
+            <button className="border-2 border-gray-400 border-solid rounded-[1vw] p-3 mb-3 w-32" type="button" onClick={() => onChange("Spring")}>Spring</button>
         </div>
     );
 }
