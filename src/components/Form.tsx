c4aeef68db9f62420b8e9e631498ed38de452e27
Kaival Shah <kaival.s.shah@gmail.com>
2025-10-13 23:59:46 -0500

    form valid

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,59 @@
 import type { Course } from '../types';
 import { useNavigate } from '@tanstack/react-router';
+import { useForm } from "react-hook-form";
+import { useState } from 'react';
+import { z } from 'zod';
+import { hasConflict } from '../utilities/checkConflicts';
+import { zodResolver } from '@hookform/resolvers/zod'
 
 interface FormProps {
     course: Course;
 }
 
+export const courseValidationSchema = z.object({
+    term: z.string().min(1).regex(/^(Fall|Winter|Spring|Summer)$/i, 'incorrect term'),
+    title: z.string().min(2, 'title should be min of 2 chars'),
+    number: z.string().regex(/^\d{3}(-\d)?$/, 'invalid course number'),
+    meets: z.string().regex(/^(?:M|Tu|W|Th|F){1,5}\s\d{1,2}:\d{2}-\d{1,2}:\d{2}$/, 'invalid meeting time')
+})
+
 const Form = ({course}: FormProps) => {
     const navigate = useNavigate();
-
-    function onSubmit() {
-    }
+    const {register, handleSubmit, formState: {errors},} = useForm<z.infer<typeof courseValidationSchema>>({
+        resolver: zodResolver(courseValidationSchema),
+        defaultValues: {
+            term: course.term,
+            meets: course.meets,
+            title: course.title,
+            number: course.number
+        }
+    });
+    const [data, setData] = useState("");
 
     return (
         <div className='flex items-center justify-center h-screen '>
-            <form className='bg-gray-200 p-10 rounded border'>
+            
+            <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))} className='bg-gray-200 p-10 rounded border'>
                 <div className='mb-5 flex flex-col'>
                     <h1 className='font-semibold'>Term</h1>
-                    <input className='text-gray-800 block border rounded pl-1' value={course.term}></input>
+                    <input {...register("term")} className='text-gray-800 block border rounded pl-1'/>
                 </div>
                 <div className='mb-5 flex flex-col'>
                     <h1 className='font-semibold'>Meets</h1>
-                    <input className='text-gray-800 block border rounded pl-1' value={course.meets}></input>
+                    <input {...register("meets")} className='text-gray-800 block border rounded pl-1'/>
                 </div>
                 <div className='mb-5 flex flex-col'>
                     <h1 className='font-semibold'>Title</h1>
-                    <input className='text-gray-800 block border rounded pl-1' value={course.title}></input>
+                    <input {...register("title")} className='text-gray-800 block border rounded pl-1'/>
                 </div>
                 <div className='mb-5 flex flex-col'>
                     <h1 className='font-semibold'>Number</h1>
-                    <input className='text-gray-800 block border rounded pl-1' value={course.number}></input>
+                    <input {...register("number")} className='text-gray-800 block border rounded pl-1'/>
                 </div>
+                {errors.term && <p>Error in form inputs</p>} 
                 <div className='flex flex-row items-center justify-center gap-8'>
                     <button onClick={() => navigate({to: '/'})} className='p-2 border rounded'>Cancel</button>
-                    <button onClick={() => onSubmit()} className='p-2 border rounded'>Submit</button>
+                    <input type={"submit"} value={"Submit"} className='p-2 border rounded'/>
                 </div>
             </form>
         </div>
